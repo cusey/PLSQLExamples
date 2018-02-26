7b87cb8ab859e3be6b5355922ec442ed8958a710
John Cusey <jjcusey@gmail.com>
2018-02-26 13:42:02 -0500

    Add files via upload

---
+++ CursorBasedRecords.sql
@@ -0,0 +1,17 @@
+DECLARE 
+   CURSOR c_employees is 
+      SELECT 
+            FIRST_NAME  || ' ' || LAST_NAME as name , 
+            EMAIL 
+      FROM EMPLOYEES WHERE EMPLOYEE_ID > 105; 
+      
+   employees_record c_employees%rowtype; 
+BEGIN 
+   OPEN c_employees; 
+   LOOP 
+      FETCH c_employees into employees_record; 
+      EXIT WHEN c_employees%notfound; 
+      DBMS_OUTPUT.put_line(employees_record.name || ' ' || employees_record.EMAIL); 
+   END LOOP; 
+END; 
+/
