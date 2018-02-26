4fb9481bdf223259f16a5d98d057014d2552700b
John Cusey <jjcusey@gmail.com>
2018-02-26 17:19:03 -0500

    Update ExplicitCursors.sql

--- ExplicitCursors.sql
+++ ExplicitCursors.sql
@@ -1,9 +1,17 @@
+--OUTPUT
+/*
+Steven King
+Neena Kochhar
+Lex De Haan
+Alexander Hunold
+Bruce Ernst
+*/
 DECLARE 
    v_first_name EMPLOYEES.FIRST_NAME%type; 
    v_last_name EMPLOYEES.LAST_NAME%type; 
 
    CURSOR c_employees is 
-      SELECT FIRST_NAME, LAST_NAME FROM EMPLOYEES; 
+      SELECT FIRST_NAME, LAST_NAME FROM EMPLOYEES WHERE EMPLOYEE_ID < 105; 
 BEGIN 
    OPEN c_employees; 
    LOOP 
