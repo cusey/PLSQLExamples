3bbc51e189880e3461269f4efe0e99d5f65f3bff
John Cusey <jjcusey@gmail.com>
2018-02-26 12:48:53 -0500

    Add files via upload

--- ImplicitCursors.sql
+++ ImplicitCursors.sql
@@ -3,10 +3,10 @@ DECLARE
     v_phone EMPLOYEES.PHONE_NUMBER%TYPE;
 
 BEGIN
-    SELECT PHONE_NUMBER INTO v_phone FROM EMPLOYEE WHERE EMPLOYEE_ID = 100;
+	SELECT PHONE_NUMBER INTO v_phone FROM EMPLOYEE WHERE EMPLOYEE_ID = 100;
 	
-    --there should only be one row returned because I am using primary key 
-    dbms_output.put_line( 'NUMBER ROWS : ' || sql%ROWCOUNT);
+	--there should only be one row returned because I am using primary key
+	dbms_output.put_line( 'NUMBER ROWS : ' || sql%ROWCOUNT);
 
 END;
 /
